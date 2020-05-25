301a49c8bb4fc97f7f31115784d09d41d917967d
allenplay1124 <jailin1124@gmail.com>
2020-05-25 17:55:18 +0800

    調整系統架構

---
+++ src/app/Database/Migrations/2020-05-25-095444_add_member.php
@@ -0,0 +1,18 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddMember extends Migration
+{
+	public function up()
+	{
+		//
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+	}
+}
