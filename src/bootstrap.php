2d77a388ce33de8bc35db4aefc2adbd58bc4a4bb
allenplay1124 <jailin1124@gmail.com>
2020-05-25 21:46:11 +0800

    修改 Eloquent ORM 參數設定

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => getenv('database.default.DBDriver'),
+    'driver'    => getenv('database.default.ELDdriver'),
     'host'      => getenv('database.default.hostname'),
     'database'  => getenv('database.default.database'),
     'username'  => getenv('database.default.username'),
