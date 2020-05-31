d23c9a024d51e1496055df163c16686f01bdf8e9
allenplay1124 <jailin1124@gmail.com>
2020-05-31 21:28:45 +0800

    加入角色

--- src/app/Database/Migrations/2020-05-25-095444_add_member.php
+++ src/app/Database/Migrations/2020-05-25-095444_add_member.php
@@ -34,22 +34,34 @@ class AddMember extends Migration
                 'default' => '',
                 'comment' => 'EMAIL'
             ],
+            'role_id' => [
+                'type' => 'int',
+                'null' => false,
+                'default' => 0,
+                'comment' => '角色id'
+            ],
             'status' => [
                 'type' => 'ENUM',
                 'constraint' => ['unactive', 'active', 'suspension'],
                 'default' => 'unactive',
-                'comment' => '狀態'
+                'comment' => '狀態：unactive 未啟用, active 啟用, suspension 停權'
             ],
-            'created_at' => [
-                'type' => 'TIMESTAMP',
+            'lang' => [
+                'type' => 'varchar',
+                'constraint' => 5,
                 'null' => false,
-                'comment' => '新增時間'
+                'default' => 'CN',
+                'comment' => '語言'
             ],
-            'updated_at' => [
-                'type' => 'TIMESTAMP',
-                'null' => false,
-                'comment' => '最後更新時間'
+            'currency' => [
+                'type' => 'varchar',
+                'constraint' => 3,
+                'null' => true,
+                'default' => 'CNY',
+                'comment' => '幣別'
             ],
+            "created_at TIMESTAMP default CURRENT_TIMESTAMP COMMENT '新增時間'",
+            "updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最後更新時間'",
             'deleted_at' => [
                 'type' => 'TIMESTAMP',
                 'null' => true,
@@ -61,6 +73,7 @@ class AddMember extends Migration
         $attributes = ['ENGINE' => 'InnoDB', 'COMMENT' => '會員帳號'];
         $this->forge->addPrimaryKey('id');
         $this->forge->addUniqueKey('username');
+        $this->forge->addKey(['username', 'email', 'status', 'role_id']);
         $this->forge->createTable('members', true, $attributes);
     }
 
